65d940f50f48d7579eeee196f459cd0f3b1a8422
Guruprasad R <guruprasad@versa-networks.com>
2021-05-16 22:43:43 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import cv2
+import numpy as np
+
+img = np.zeros((512,512,3), np.uint8)
+print(img)
+
+#img[:] = 255,0,0,
+
+# image starting pt end pt and color, last is thickness
+#cv2.line(img, (0,0), (300,300), (0,255,0), 3)
+
+cv2.line(img, (0,0), (img.shape[1],img.shape[0]), (0,255,0), 3)
+
+# With thickness
+cv2.rectangle(img, (0,0), (250,350), (0,0,255), 2)
+
+# With rectangle filled
+#cv2.rectangle(img, (0,0), (250,350), (0,0,255), cv2.FILLED)
+
+cv2.circle(img, (400,50), 30, (255,255,0),5)
+
+cv2.putText(img, "OPEN CV ", (100,100), cv2.FONT_HERSHEY_COMPLEX, 1, (0,150,0),1)
+
+cv2.imshow("Image", img)
+
+
+cv2.waitKey(0)
