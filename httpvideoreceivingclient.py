55fa16c5270d28f61ea25ff3e1d1d48f920ba08a
Guruprasad <Guruprasad>
2021-08-18 01:52:51 +0530

    video web streamer

---
+++ httpvideoreceivingclient.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+import urllib.request
+
+
+url = 'http://127.0.0.1:5000/video_feed'
+
+camera = cv2.VideoCapture(0)
+
+while True:
+   #print('Getting image')
+    req = urllib.request.Request(url)
+
+
+    with urllib.request.urlopen(req) as resp:
+        data = resp.read()
+
+        #print('Got data')
+        #print(type(data))
+
+
+    # Numpy to convert into a array
+
+    #print('Got image')
+    # Finally decode the array to OpenCV usable format ;)
+    #imgTest = cv2.imdecode(imgNp, -1)
+    #imgTest = cv2.resize(imgTest, (640, 480))
+
+    #imgTest = cv2.flip(imgTest, 1)
+
+    imgNp = np.array(bytearray(data), dtype=np.uint8)
+    imgTest = cv2.imdecode(imgNp, -1)
+
+    imgTest = cv2.resize(imgTest, (640, 480))
+
+    cv2.imshow("Video", imgTest)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
---
+++ videowebstreamer.py
@@ -0,0 +1,32 @@
+from flask import Flask, stream_with_context, render_template, Response
+import cv2
+#Initialize the Flask app
+app = Flask(__name__)
+
+camera = cv2.VideoCapture(0)
+
+def gen_frames():
+    while True:
+        success, frame = camera.read()  # read the camera frame
+        if not success:
+            pass
+        else:
+            frame = cv2.flip(frame, 1)
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            yield frame
+        #yield (b'--frame\r\n'
+         #      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen_frames(),mimetype='multipart/x-mixed-replace; boundary=frame')
+    #return gen_frames()
+
+if __name__ == "__main__":
+    app.run(debug=True)
