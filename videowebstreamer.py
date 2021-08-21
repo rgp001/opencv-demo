7c7bf746fe8e7bdb112fda60eaf2a2e0f2da492b
Guruprasad <rgp001@gmail.com>
2021-08-21 19:31:15 +0530

    Commenting

--- videowebstreamer.py
+++ videowebstreamer.py
@@ -3,18 +3,17 @@ import cv2
 #Initialize the Flask app
 app = Flask(__name__)
 
-camera = cv2.VideoCapture(0)
+#camera = cv2.VideoCapture(0)
 
 def gen_frames():
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            pass
-        else:
-            frame = cv2.flip(frame, 1)
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield frame
+    success, frame = camera.read()  # read the camera frame
+    if not success:
+        pass
+    else:
+        frame = cv2.flip(frame, 1)
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield frame
         #yield (b'--frame\r\n'
          #      b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
 
